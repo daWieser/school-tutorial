86160e0f59505228e3f2b94a4a1ad0348f58cb4c
Benjamin Wieser <benjamin.wieser.ext@porscheinformatik.com>
2024-06-19 10:12:21 +0200

    new pictures

--- src/app/initial-state.ts
+++ src/app/initial-state.ts
@@ -1,86 +1,86 @@
 export const initialState = [
-    {
-      id: 1,
-      picture: "original_1.webp",
-      connectedId: 12,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 2,
-      picture: "original_2.webp",
-      connectedId: 11,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 3,
-      picture: "original_3.webp",
-      connectedId: 10,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 4,
-      picture: "original_4.webp",
-      connectedId: 9,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 5,
-      picture: "original_5.webp",
-      connectedId: 8,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 6,
-      picture: "original_6.webp",
-      connectedId: 7,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 7,
-      picture: "original_6.webp",
-      connectedId: 6,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 8,
-      picture: "original_5.webp",
-      connectedId: 5,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 9,
-      picture: "original_4.webp",
-      connectedId: 4,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 10,
-      picture: "original_3.webp",
-      connectedId: 3,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 11,
-      picture: "original_2.webp",
-      connectedId: 2,
-      open: false,
-      forcedState:false
-    },
-    {
-      id: 12,
-      picture: "original_1.webp",
-      connectedId: 1,
-      open: false,
-      forcedState:false
-    },
-  ]
+  {
+    id: 1,
+    picture: "original_7.jpg",
+    connectedId: 12,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 2,
+    picture: "original_8.jpg",
+    connectedId: 11,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 3,
+    picture: "original_9.jpg",
+    connectedId: 10,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 4,
+    picture: "original_4.jpg",
+    connectedId: 9,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 5,
+    picture: "original_5.jpg",
+    connectedId: 8,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 6,
+    picture: "original_6.jpg",
+    connectedId: 7,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 7,
+    picture: "original_6.jpg",
+    connectedId: 6,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 8,
+    picture: "original_5.jpg",
+    connectedId: 5,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 9,
+    picture: "original_4.jpg",
+    connectedId: 4,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 10,
+    picture: "original_9.jpg",
+    connectedId: 3,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 11,
+    picture: "original_8.jpg",
+    connectedId: 2,
+    open: false,
+    forcedState:false
+  },
+  {
+    id: 12,
+    picture: "original_7.jpg",
+    connectedId: 1,
+    open: false,
+    forcedState:false
+  },
+]
